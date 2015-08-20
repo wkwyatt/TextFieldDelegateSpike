aa338fbd32c7b16d9c70ed426237ff8611b94659
wkwyatt <wkwyatt@gmail.com>
2015-08-19 21:09:58 -0400

    cash format for text field

--- TextFieldDelegateSpike/CashTextFieldDelegate.swift
+++ TextFieldDelegateSpike/CashTextFieldDelegate.swift
@@ -16,5 +16,53 @@ import Foundation
 import UIKit
 
 class CashTextFieldDelegate: NSObject, UITextFieldDelegate {
+
     
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        var oldText  = textField.text as NSString
+        var newText = oldText.stringByReplacingCharactersInRange(range, withString: string)
+        var newTextString = String(newText)
+        
+        let digits = NSCharacterSet.decimalDigitCharacterSet()
+        var digitText = ""
+        for c in newTextString.unicodeScalars {
+            if digits.longCharacterIsMember(c.value) {
+                digitText.append(c)
+            }
+        }
+        
+        
+        // TODO: format string to cash value
+        if let pennies = digitText.toInt() {
+            newText = "$" + self.dollarStringFromInt(pennies) + "." + self.centsStringFromInt(pennies)
+        } else {
+                newText = "$0.00"
+        }
+        
+        textField.text = newText
+        
+        return true
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if textField.text.isEmpty {
+            textField.text = "$0.00"
+        }
+    }
+    
+    func dollarStringFromInt(value: Int) -> String {
+        return String(value/100)
+    }
+    
+    func centsStringFromInt(value: Int) -> String {
+        let cents = value % 100
+        var centsString = String(cents)
+        
+        if cents < 10 {
+            centsString = "0" + centsString
+        }
+        
+        return centsString
+    }
 }
